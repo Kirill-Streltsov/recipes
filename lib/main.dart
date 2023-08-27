a91ca112213d5ca6587e3568d09f51693c7d9818
Kirill-Streltsov <krlsrlsv@gmail.com>
2023-08-27 14:51:56 +0200

    finished App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:recipes/recipe_detail.dart';
+import 'recipe.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -42,17 +44,61 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title)
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: SafeArea(
-        child: Container(),
-      ),
+          child: ListView.builder(
+              itemCount: Recipe.samples.length,
+              itemBuilder: (BuildContext context, int index) {
+                Widget buildRecipeCard(Recipe recipe) {
+                  return Card(
+                    elevation: 2.0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: <Widget>[
+                          Image(image: AssetImage(recipe.imageUrl)),
+                          // 5
+                          const SizedBox(
+                            height: 14.0,
+                          ),
+
+                          Text(
+                            recipe.label,
+                            style: const TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w700,
+                              fontFamily: 'Palatino',
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  );
+                }
+
+                return GestureDetector(
+                  onTap: () {
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          // 10
+                          return RecipeDetail(recipe: Recipe.samples[index]);
+                        },
+                      ),
+                    );
+                  },
+
+                  child: buildRecipeCard(Recipe.samples[index]),
+                );
+              })),
     );
   }
 }
